d6142f1d297acb2295526e058ec2e3f8ac58b921
Bailey Tincher <btincher99@gmail.com>
2021-05-05 12:42:17 -0500

    Create utility functions to fetch commit messages

---
+++ scripts/gen_progress_report.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import os
+import requests
+
+import arrow
+
+def get_commits(repo_name: str, since: str) -> List:
+  commits = requests.get(f'https://api.github.com/repos/{repo_name}/commits?since={since}').json()
+
+  def parse_commit(commit):
+    message = commit['commit']['message']
+    if message.startswith('Merge pull request'):
+      message = message.split('\n')[1:]
+      message = '\n'.join(message)
+      message = message.strip()
+
+    return {
+      'message': message,
+      'url': commit['commit']['url'],
+      'date': commit['commit']['committer']['date']
+    }
+  return list(map(parse_commit, commits))
+
+if __name__ == '__main__':
+  last_week = arrow.utcnow().shift(weeks=-1).isoformat().split('.')[0] + 'Z'
+  desktop_commits = get_commits('actually-colab/desktop', since=last_week)
+  editor_commits = get_commits('actually-colab/editor', since=last_week)
