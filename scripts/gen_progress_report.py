a9144d68d5e3c07778fd1cceb797118888965206
Bailey Tincher <btincher99@gmail.com>
2021-05-06 09:59:51 -0500

    Draft report generation

--- scripts/gen_progress_report.py
+++ scripts/gen_progress_report.py
@@ -22,7 +22,15 @@ def get_commits(repo_name: str, since: str) -> List:
     }
   return list(map(parse_commit, commits))
 
+def create_md(path: str, title: str, commits: List) -> str:
+  title = f"# {title}"
+  commits = [f"- [{commit['date']}]({commit['url']}): {commit['message']}" for commit in commits]
+  contents = title + '\n\n' + '\n\n'.join(commits) 
+  with open(path, mode='x') as f:
+    f.writelines(contents)
+
 if __name__ == '__main__':
   last_week = arrow.utcnow().shift(weeks=-1).isoformat().split('.')[0] + 'Z'
   desktop_commits = get_commits('actually-colab/desktop', since=last_week)
+  create_md('test.md', 'Desktop Progress', desktop_commits)
   editor_commits = get_commits('actually-colab/editor', since=last_week)
